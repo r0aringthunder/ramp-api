452cf62b4bd16eab3674f927f6b955b1acd3d4c1
Will Mercer <45526202+r0aringthunder@users.noreply.github.com>
2024-03-30 14:06:28 -0500

    Added comments to the config

--- src/config/ramp.php
+++ src/config/ramp.php
@@ -1,8 +1,14 @@
 <?php
 
 return [
+    // Your Ramp API keys
     'client_id' => env('RAMP_CLIENT_ID', 'your_client_id'),
     'client_secret' => env('RAMP_CLIENT_SECRET', 'your_client_secret'),
+    
+    // False will put you in the Ramp sandbox but true will interact with production Ramp data
     'prod_ready' => env('PROD_READY', false),
+
+    // The package defaults to all possible scopes so that you can test without an issue on the Ramp sandbox.
+    // I reccomend that you identify what scopes you need for production before release.
     'scopes' => env('RAMP_SCOPES', 'accounting:read accounting:write bills:read business:read cards:read cards:write cashbacks:read departments:read departments:write entities:read leads:read leads:write limits:read limits:write locations:read locations:write memos:read merchants:read receipt_integrations:read receipt_integrations:write receipts:read reimbursements:read spend_programs:read spend_programs:write statements:read transactions:read transfers:read users:read users:write'),
 ];
